3a2b8b556dc309b8301eb330a685dfd59a9dfe1c
vy3191 <venkyafhs@gmail.com>
2020-03-12 19:06:58 -0400

    Added validation middleware

---
+++ middleware/validation.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcryptjs');
+const userModal = require('../users/users-model');
+
+function validation(req,res,next) {
+   try{
+     const user = userModal.findBy({username}).first();
+     const {username, password} = req.headers;
+
+     if(!username) res.status(401).json({msg:'User name is missing'});
+     if(!password) res.status(401).json({msg:'Password is missing'});
+     if(!user) res.status(401).json({msg:'User does not exis'});
+
+     const isPasswordValid = await bcrypt.compare(password, user.password);
+     
+     if(!isPasswordValid) res.status(403).json({msg:'Wrong credentials-please correct credentials'});
+     next();
+   }catch(err){
+     next(err);
+   }
+}
+
+module.exports = validation;
