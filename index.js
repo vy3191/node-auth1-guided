2337922d1214f932e38442a2dd430b8ba38b38e9
vy3191 <venkyafhs@gmail.com>
2020-03-18 14:54:12 -0400

    Completed guided project

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ server.use(session({
 	 name:'token', //overwrite the default cookie name, hides our stack 
 	 resave: false,
 	 saveUninitialized:false, // GDPR laws against setting cookies automatically.
-	 secret: "keep it secret it secret, keep it safe",
+	 secret: process.env.COOKIE_SECRET || "secret"
 	 cookie: {
 		//  maxAge: 15 * 1000,
 		 httpOnly: true,
